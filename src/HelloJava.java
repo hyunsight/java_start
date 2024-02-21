790a28ba2e8efe5b59be09f88d2727bc5c2de05f
hyunsight <blueskytjgus@gmail.com>
2024-02-22 00:31:53 +0900

    신규 생성

---
+++ src/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+    //public static void main: 프로그램의 시작점 (클래스 안의 method)
+    // - 해당 시작점을 찾아서 실행
+    // - 해당 클래스 안의 method는 4번 들여쓰기(스페이스 4번/tab) 통해 구분 (가독성 위해)
+    // - 해당 method도 중괄호로 본인의 어떤 범위를 지정 (시작 ~ 끝)
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
