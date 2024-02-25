a1a2bcdccea8b24567f5c619f1109142158445ce
hyunsight <blueskytjgus@gmail.com>
2024-02-26 00:23:01 +0900

    24.02.26

---
+++ src/CommentJava.java
@@ -0,0 +1,17 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        // 주석
+        // : 작성된 내용을 자바에서 무시
+        // : 사람이 읽기 위해 작성 > 어떤 목적으로 만들어진 코드다 등의 내용 작성
+        //  - 한 줄 주석(single line comment)
+        System.out.println("hello java1"); //hello java1을 출력합니다.
+        //System.out.println("hello java2");
+
+        //  - 여러 줄 주석(multi line comment)
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+    }
+}
