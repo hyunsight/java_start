13d4cbb4b10f89402f349636cc9b0bbab557e76b
hyunsight <blueskytjgus@gmail.com>
2024-02-27 23:53:44 +0900

    24.02.27

---
+++ src/variable/ex/VarEx2.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx2 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2; //num1, num2 값 읽기 > 30 계산 > sum 대입
+        System.out.println(sum);
+    }
+}
---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        //soutv: v는 'value' 의미
+        System.out.println("longVar = " + longVar);
+
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+    }
+}
